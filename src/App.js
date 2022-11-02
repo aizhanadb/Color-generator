8563fc4acd9631b96e16f9a222607447e305ec6a
aizhan <106273107+aizhanadb@users.noreply.github.com>
2022-11-02 04:34:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Values from "values.js";
+import SingleColor from "./components/SingleColor";
+import "./styles.css";
+
+export default function App() {
+  const [color, setColor] = useState([]);
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15205").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.error(error);
+    }
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
